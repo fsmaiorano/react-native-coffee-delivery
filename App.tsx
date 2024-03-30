1ace05dbe8bdb8799354adc4ac8704b5051569ad
Fábio Maiorano <fsmaiorano@gmail.com>
2024-03-30 17:10:07 +0000

    Update StatusBar background color

--- App.tsx
+++ App.tsx
@@ -2,13 +2,14 @@ import { StatusBar } from "react-native";
 import "react-native-gesture-handler";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Routes } from "./src/routes";
+import { THEME } from "./src/styles/theme";
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar
         barStyle="light-content"
-        backgroundColor="transparent"
+        backgroundColor={THEME.COLORS.GREY_900}
         translucent
       />
       <Routes />
