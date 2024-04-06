cc2440e143bac0fc4f104217ddd67ac282344b90
fsmaiorano <fabio.maiorano@armis.pt>
2024-04-06 20:32:45 +0100

    Add header customization and button in detail screen

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,7 +1,9 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import { Button } from "react-native";
 import { Detail } from "../screens/Detail";
 import { Home } from "../screens/Home";
+import { THEME } from "../styles/theme";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -14,7 +16,29 @@ export function AppRoutes() {
       }}
     >
       <Screen name="home" component={Home} />
-      <Screen name="detail" component={Detail} options={{}} />
+      <Screen
+        name="detail"
+        component={Detail}
+        options={{
+          headerShown: true,
+          headerTitle: "",
+          headerBlurEffect: "light",
+          headerBackButtonMenuEnabled: true,
+          customAnimationOnGesture: true,
+          headerBackTitleVisible: false,
+          headerTintColor: "#fff",
+          headerStyle: {
+            backgroundColor: THEME.COLORS.GREY_900,
+          },
+          headerRight: () => (
+            <Button
+              onPress={() => alert("This is a button!")}
+              title="Info"
+              color="#fff"
+            />
+          ),
+        }}
+      />
     </Navigator>
   );
 }
