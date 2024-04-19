5640450b8aba2f4066a88b0c126543ad8f2075aa
fsmaiorano <fabio.maiorano@armis.pt>
2024-04-19 18:04:26 +0100

    wip coffee details

--- src/screens/Detail/index.tsx
+++ src/screens/Detail/index.tsx
@@ -1,34 +1,34 @@
-import { useRoute } from "@react-navigation/native";
-import { Text, View } from "react-native";
-import { Coffee } from "../../context/AppContext";
-import { styles } from "./styles";
+import {useRoute} from "@react-navigation/native";
+import {Text, View} from "react-native";
+import {Coffee} from "../../context/AppContext";
+import {styles} from "./styles";
 
 interface Params {
-  coffee: Coffee;
+    coffee: Coffee;
 }
 
 export function Detail() {
-  const route = useRoute();
-  const { coffee } = route.params as Params;
-  const handlePrice = (price: number) => {
-    return price.toFixed(2).replace(".", ",");
-  };
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text>
-          {coffee.tags &&
-            coffee.tags.map((tag, index) => (
-              <View key={index}>
-                <Text>{tag}</Text>
-              </View>
-            ))}
-        </Text>
-      </View>
-      <View style={styles.footer}>
-        <Text>dsadsa</Text>
-      </View>
-      {/* <Image
+    const route = useRoute();
+    const {coffee} = route.params as Params;
+    const handlePrice = (price: number) => {
+        return price.toFixed(2).replace(".", ",");
+    };
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <View style={styles.tagContainer}>
+                    {coffee.tags &&
+                        coffee.tags.map((tag, index) => (
+                            <View key={index} style={styles.tag}>
+                                <Text style={styles.tagText}>{tag}</Text>
+                            </View>
+                        ))}
+                </View>
+            </View>
+            <View style={styles.footer}>
+                <Text>dsadsa</Text>
+            </View>
+            {/* <Image
         source={imageMapper[coffee.imageSrc as keyof typeof imageMapper]}
       />
       <Text>{coffee.title}</Text>
@@ -37,6 +37,6 @@ export function Detail() {
         <Text>R$ </Text>
         <Text>{handlePrice(coffee.value)}</Text>
       </Text> */}
-    </View>
-  );
+        </View>
+    );
 }
--- src/screens/Detail/styles.ts
+++ src/screens/Detail/styles.ts
@@ -1,17 +1,62 @@
-import { StyleSheet } from "react-native";
-import { THEME } from "../../styles/theme";
+import {StyleSheet} from "react-native";
+import {THEME} from "../../styles/theme";
 
 export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column"
-  },
-  header: {
-    flex: 2,
-    backgroundColor: THEME.COLORS.GREY_900,
-  },
-  footer: {
-    flex: 1,
-    backgroundColor: THEME.COLORS.WHITE,
-  },
+    container: {
+        flex: 1,
+        flexDirection: "column"
+    },
+    header: {
+        flex: 2,
+        backgroundColor: THEME.COLORS.GREY_900,
+    },
+    footer: {
+        flex: 1,
+        backgroundColor: THEME.COLORS.WHITE,
+    },
+    tagContainer: {
+        flexDirection: "row",
+        flexWrap: "wrap",
+        alignItems: "center",
+    },
+    tag: {
+        justifyContent: "center",
+        alignItems: "center",
+        width: 115,
+        height: 30,
+        borderRadius: 50,
+        marginTop: 12,
+        marginLeft: 12,
+        borderWidth: 1,
+        borderColor: THEME.COLORS.GREY_800,
+        backgroundColor: THEME.COLORS.GREY_800,
+    },
+    tagText: {
+        fontSize: 14,
+        fontWeight: "bold",
+        color: THEME.COLORS.WHITE,
+        fontFamily: THEME.FONTS.ROBOTO_BOLD,
+    },
 });
+
+//
+// coffeeTag: {
+//     width: 85,
+//         height: 30,
+//         justifyContent: "center",
+//         alignItems: "center",
+//         borderRadius: 50,
+//         backgroundColor: THEME.COLORS.PURPLE_300,
+//         marginRight: 8,
+//         borderWidth: 0,
+//         borderColor: THEME.COLORS.PURPLE_700,
+// },
+// coffeeSelectedTag: {
+//     borderWidth: 1,
+// },
+// coffeeTagText: {
+//     fontSize: 10,
+//         fontWeight: "bold",
+//         color: THEME.COLORS.PURPLE_700,
+//         fontFamily: THEME.FONTS.ROBOTO_BOLD,
+// },
