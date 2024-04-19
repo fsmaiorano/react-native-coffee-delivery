b689a997faf3e644f6e51f6574a593d5580899ab
fsmaiorano <fabio.maiorano@armis.pt>
2024-04-19 18:22:14 +0100

    Refactor Products component to use FlatList for rendering coffee items

--- src/screens/Detail/index.tsx
+++ src/screens/Detail/index.tsx
@@ -1,34 +1,41 @@
-import {useRoute} from "@react-navigation/native";
-import {Text, View} from "react-native";
-import {Coffee} from "../../context/AppContext";
-import {styles} from "./styles";
+import { useRoute } from "@react-navigation/native";
+import { Text, View } from "react-native";
+import { Coffee } from "../../context/AppContext";
+import { styles } from "./styles";
 
 interface Params {
-    coffee: Coffee;
+  coffee: Coffee;
 }
 
 export function Detail() {
-    const route = useRoute();
-    const {coffee} = route.params as Params;
-    const handlePrice = (price: number) => {
-        return price.toFixed(2).replace(".", ",");
-    };
-    return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <View style={styles.tagContainer}>
-                    {coffee.tags &&
-                        coffee.tags.map((tag, index) => (
-                            <View key={index} style={styles.tag}>
-                                <Text style={styles.tagText}>{tag}</Text>
-                            </View>
-                        ))}
-                </View>
-            </View>
-            <View style={styles.footer}>
-                <Text>dsadsa</Text>
-            </View>
-            {/* <Image
+  const route = useRoute();
+  const { coffee } = route.params as Params;
+  const handlePrice = (price: number) => {
+    return price.toFixed(2).replace(".", ",");
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.tagContainer}>
+          {coffee.tags &&
+            coffee.tags.map((tag, index) => (
+              <View key={index} style={styles.tag}>
+                <Text style={styles.tagText}>{tag}</Text>
+              </View>
+            ))}
+        </View>
+        <View style={styles.coffeeContainer}>
+          <Text style={styles.coffeeTitle}>{coffee.title}</Text>
+          <View style={styles.coffeePriceWrapper}>
+            <Text style={styles.coffeePricePrefix}>R$</Text>
+            <Text style={styles.coffeePrice}>{handlePrice(coffee.value)}</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.footer}>
+        <Text>dsadsa</Text>
+      </View>
+      {/* <Image
         source={imageMapper[coffee.imageSrc as keyof typeof imageMapper]}
       />
       <Text>{coffee.title}</Text>
@@ -37,6 +44,6 @@ export function Detail() {
         <Text>R$ </Text>
         <Text>{handlePrice(coffee.value)}</Text>
       </Text> */}
-        </View>
-    );
+    </View>
+  );
 }
--- src/screens/Detail/styles.ts
+++ src/screens/Detail/styles.ts
@@ -1,42 +1,77 @@
-import {StyleSheet} from "react-native";
-import {THEME} from "../../styles/theme";
+import { StyleSheet } from "react-native";
+import { THEME } from "../../styles/theme";
 
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: "column"
-    },
-    header: {
-        flex: 2,
-        backgroundColor: THEME.COLORS.GREY_900,
-    },
-    footer: {
-        flex: 1,
-        backgroundColor: THEME.COLORS.WHITE,
-    },
-    tagContainer: {
-        flexDirection: "row",
-        flexWrap: "wrap",
-        alignItems: "center",
-    },
-    tag: {
-        justifyContent: "center",
-        alignItems: "center",
-        width: 115,
-        height: 30,
-        borderRadius: 50,
-        marginTop: 12,
-        marginLeft: 12,
-        borderWidth: 1,
-        borderColor: THEME.COLORS.GREY_800,
-        backgroundColor: THEME.COLORS.GREY_800,
-    },
-    tagText: {
-        fontSize: 14,
-        fontWeight: "bold",
-        color: THEME.COLORS.WHITE,
-        fontFamily: THEME.FONTS.ROBOTO_BOLD,
-    },
+  container: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  header: {
+    flex: 2,
+    backgroundColor: THEME.COLORS.GREY_900,
+  },
+  footer: {
+    flex: 1,
+    backgroundColor: THEME.COLORS.WHITE,
+  },
+  tagContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "center",
+    marginTop: 12,
+    marginLeft: 12,
+  },
+  tag: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 115,
+    height: 30,
+    borderRadius: 50,
+
+    borderWidth: 1,
+    borderColor: THEME.COLORS.GREY_800,
+    backgroundColor: THEME.COLORS.GREY_800,
+  },
+  tagText: {
+    fontSize: 14,
+    fontWeight: "bold",
+    color: THEME.COLORS.WHITE,
+    fontFamily: THEME.FONTS.ROBOTO_BOLD,
+  },
+  coffeeContainer: {
+    flex: 1,
+    margin: 14,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: THEME.COLORS.GREY_900,
+  },
+  coffeeImage: {
+    width: "100%",
+    height: "100%",
+  },
+  coffeeTitle: {
+    fontSize: 26,
+    lineHeight: 70,
+    color: THEME.COLORS.WHITE,
+    fontFamily: THEME.FONTS.BALOO2_BOLD,
+  },
+  coffeePriceWrapper: {
+    flexDirection: "row",
+    backgroundColor: THEME.COLORS.GREY_900,
+    textAlign: "justify"
+  },
+  coffeePricePrefix: {
+    color: THEME.COLORS.YELLOW_700,
+    fontFamily: THEME.FONTS.BALOO2_BOLD,
+    lineHeight: 75,
+    marginRight: 4,
+    fontSize: 20,
+  },
+  coffeePrice: {
+    color: THEME.COLORS.YELLOW_700,
+    fontFamily: THEME.FONTS.BALOO2_BOLD,
+    fontSize: 40,
+  },
 });
 
 //
