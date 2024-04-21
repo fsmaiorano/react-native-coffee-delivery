e5d3125a19a66d3f5d852976b631bedf1171839e
Fábio Maiorano <fsmaiorano@gmail.com>
2024-04-21 01:08:03 +0100

    wip details layout

--- src/screens/Detail/index.tsx
+++ src/screens/Detail/index.tsx
@@ -1,49 +1,46 @@
-import { useRoute } from "@react-navigation/native";
-import { Text, View } from "react-native";
-import { Coffee } from "../../context/AppContext";
-import { styles } from "./styles";
+import {useRoute} from "@react-navigation/native";
+import {Image, Text, View} from "react-native";
+import {Coffee} from "../../context/AppContext";
+import {styles} from "./styles";
+
+const coffeeImage = "../../assets/coffee.png";
 
 interface Params {
-  coffee: Coffee;
+    coffee: Coffee;
 }
 
 export function Detail() {
-  const route = useRoute();
-  const { coffee } = route.params as Params;
-  const handlePrice = (price: number) => {
-    return price.toFixed(2).replace(".", ",");
-  };
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <View style={styles.tagContainer}>
-          {coffee.tags &&
-            coffee.tags.map((tag, index) => (
-              <View key={index} style={styles.tag}>
-                <Text style={styles.tagText}>{tag}</Text>
-              </View>
-            ))}
+    const route = useRoute();
+    const {coffee} = route.params as Params;
+    const handlePrice = (price: number) => {
+        return price.toFixed(2).replace(".", ",");
+    };
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <View style={styles.tagContainer}>
+                    {coffee.tags &&
+                        coffee.tags.map((tag, index) => (
+                            <View key={index} style={styles.tag}>
+                                <Text style={styles.tagText}>{tag}</Text>
+                            </View>
+                        ))}
+                </View>
+                <View style={styles.coffeeContainer}>
+                    <Text style={styles.coffeeTitle}>{coffee.title}</Text>
+                    <View style={styles.coffeePriceWrapper}>
+                        <Text style={styles.coffeePricePrefix}>R$</Text>
+                        <Text style={styles.coffeePrice}>{handlePrice(coffee.value)}</Text>
+                    </View>
+                </View>
+                <Text style={styles.coffeeDescription}>{coffee.description}</Text>
+            </View>
+            <View style={styles.footer}>
+                <Text>dsadsa</Text>
+            </View>
+            <View style={styles.CoffeeImageWrapper}>
+                <Image source={require(coffeeImage)} style={styles.coffeeImage}/>
+            </View>
         </View>
-        <View style={styles.coffeeContainer}>
-          <Text style={styles.coffeeTitle}>{coffee.title}</Text>
-          <View style={styles.coffeePriceWrapper}>
-            <Text style={styles.coffeePricePrefix}>R$</Text>
-            <Text style={styles.coffeePrice}>{handlePrice(coffee.value)}</Text>
-          </View>
-        </View>
-      </View>
-      <View style={styles.footer}>
-        <Text>dsadsa</Text>
-      </View>
-      {/* <Image
-        source={imageMapper[coffee.imageSrc as keyof typeof imageMapper]}
-      />
-      <Text>{coffee.title}</Text>
-      <Text>{coffee.description}</Text>
-      <Text>
-        <Text>R$ </Text>
-        <Text>{handlePrice(coffee.value)}</Text>
-      </Text> */}
-    </View>
-  );
+    );
 }
