8e65af22cab767e76f1e8848055e23c6e991b95b
Fábio Maiorano <fsmaiorano@gmail.com>
2024-04-26 18:19:45 +0100

    Refactor quantity component styles in styles.ts

--- src/components/quantity/styles.ts
+++ src/components/quantity/styles.ts
@@ -1,55 +1,54 @@
-import {Dimensions, StyleSheet} from "react-native";
-import {THEME} from "../../styles/theme";
+import { Dimensions, StyleSheet } from "react-native";
+import { THEME } from "../../styles/theme";
 
 const screenWidth = Dimensions.get("window").width;
 
 export const styles = StyleSheet.create({
-    container: {
-        flexDirection: "row",
-        borderColor: "#000",
-        borderRadius: 10,
-        marginLeft: 50,
-        marginRight: 50,
-        marginBottom: 10,
-        backgroundColor: THEME.COLORS.GREY_300
-    },
-    quantityContainer: {
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-        width: '40%',
-    },
-    quantityButton: {
-        width: 50,
-        height: 50,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    quantityButtonText: {
-        fontSize: 35,
-        fontWeight: "300",
-        color: THEME.COLORS.PURPLE_500,
-    },
-    quantity: {
-        fontSize: 20,
-    },
-    actionContainer: {
-        width: 165,
-        borderRadius: 10,
-        backgroundColor: THEME.COLORS.PURPLE_700,
-    },
-    actionButton: {
-        height: 50,
-
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    actionButtonText: {
-        fontSize: 16,
-        fontWeight: "bold",
-        color: THEME.COLORS.WHITE,
-    },
-    action: {
-        fontSize: 20,
-    },
+  container: {
+    flexDirection: "row",
+    borderColor: "#000",
+    borderRadius: 10,
+    marginLeft: 50,
+    marginRight: 50,
+    marginBottom: 10,
+    backgroundColor: THEME.COLORS.GREY_300,
+  },
+  quantityContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 115,
+  },
+  quantityButton: {
+    width: 50,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  quantityButtonText: {
+    fontSize: 35,
+    fontWeight: "300",
+    color: THEME.COLORS.PURPLE_500,
+  },
+  quantity: {
+    fontSize: 20,
+  },
+  actionContainer: {
+    width: 165,
+    borderRadius: 10,
+    backgroundColor: THEME.COLORS.PURPLE_700,
+  },
+  actionButton: {
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  actionButtonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: THEME.COLORS.WHITE,
+  },
+  action: {
+    fontSize: 20,
+  },
 });
